88f19c2374fa0787c84c81d6b21e47528eb4cc69
FernandoPucci <fsilvapucci@gmail.com>
2018-12-26 20:57:17 -0200

    Enhance performance with Promise.all() and asyncronous calls

--- node-02-async-await/index.js
+++ node-02-async-await/index.js
@@ -55,9 +55,18 @@ main()
 
 async function main() {
     try {
+        console.time('medida-promise') //start da medição do tempo
         const usuario = await obterUsuario()
-        const telefone = await obterTelefone(usuario.id)
-        const endereco = await obterEnderecoAsync(usuario.id)
+
+        //como o Telefone e o Endereço não se dependem entre si (dependem apenas do usuario)
+        //separo a promise para rodarem de forma assincrona, melhorando o tempo de execução
+        const resultado = await Promise.all([
+            obterTelefone(usuario.id),
+            obterEnderecoAsync(usuario.id)
+        ]);
+
+        const telefone = resultado[0]
+        const endereco = resultado[1]
 
         console.log('resultado', `
                  ID: ${usuario.id}
@@ -67,6 +76,8 @@ async function main() {
                  Cidade: ${endereco.cidade}/${endereco.estado}
                  Telefone: (${telefone.ddd})${telefone.numero}
                  `);
+
+        console.timeEnd('medida-promise')//end da medição do tempo
 
     } catch (error) {
         console.error("OCORREU UM ERRO! ", error)
