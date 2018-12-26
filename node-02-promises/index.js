7a873e8d9b0afb6aeef8cde4e9141d69b7f9e38a
FernandoPucci <fsilvapucci@gmail.com>
2018-12-26 20:21:25 -0200

    Add Promise

--- node-02-promises/index.js
+++ node-02-promises/index.js
@@ -7,7 +7,7 @@
 function obterUsuario() {
     // Quando houver problema - reject(erro)
     // Quando sucesso - resolve  
-    return new Promise(function resolvePromise(resolve, reject) {
+    return new Promise(function resolverPromise(resolve, reject) {
         setTimeout(function () {
             return resolve({
                 id: 31731,
@@ -19,9 +19,9 @@ function obterUsuario() {
 };
 
 function obterTelefone(idUsuario) {
-    return new Promise(function resolvePromise(resolv, reject) {
+    return new Promise(function resolverPromise(resolve, reject) {
         setTimeout(function () {
-            return resolv({
+            return resolve({
                 id: 24525,
                 ddd: '16',
                 numero: '39112154'
@@ -31,9 +31,9 @@ function obterTelefone(idUsuario) {
 }
 
 function obterEndereco(idUsuario) {
-    return new Promise(function resolvePromise(resolv, reject) {
+    return new Promise(function resolverPromise(resolve, reject) {
         setTimeout(function () {
-            return resolv(null, {
+            return resolve( {
                 logradouro: 'Rua XPTO',
                 numero: '35',
                 complemento: 'apartamento',
@@ -45,20 +45,46 @@ function obterEndereco(idUsuario) {
 }
 
 //carrega usuario
-const usuarioPromise = obterUsuario();
+const usuarioPromise = obterUsuario()
 
 // para manipular o sucesso usamos a função .then
+// para manipular erros, usamos o .catch
+// função usuario -> telefone -> telefone
 usuarioPromise
     .then(function (usuario) {
-        console.log("Resultado:", usuario);
-        return usuario;
+        return obterTelefone(usuario.id)
+            .then(function resolverTelefone(telefone) {
+                return {
+                    usuario: {
+                        id: usuario.id,
+                        nome: usuario.name
+                    },
+                    telefone: {
+                        ddd: telefone.ddd,
+                        numero: telefone.numero
+                    }
+                }
+            })
     })
     .then(function (usuario) {
-        return obterTelefone(usuario.id);
+        return obterEndereco((usuario.id))
+            .then(function resolverEndereco(endereco) {
+                return {
+                    usuario,
+                    endereco: {
+                        logradouro: endereco.logradouro,
+                        numero: endereco.numero,
+                        complemento: endereco.complemento,
+                        cidade: endereco.cidade,
+                        estado: endereco.estado
+                    }
+                }
+            })
     })
-    .then(function (usuario) {
-        return obterEndereco((usuario.id));
+    .then(function (resultado) {
+        console.log('resultado', resultado);
     })
     .catch(function erro(error) {
         console.erro("HOUVE UM ERRO! ", error);
     });
+
