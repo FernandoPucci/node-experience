cc1216beff9b5fcf6d83ebf2e5cfa0714b4e31ba
FernandoPucci <fsilvapucci@gmail.com>
2018-12-26 21:12:26 -0200

    Add stdin event

--- node-03-events/index.js
+++ node-03-events/index.js
@@ -1,3 +1,5 @@
+//PROMISES - São executadas uma unica vez
+//EVENTS - São executados para ações contínuas (ex. leitura de arquivo, leitura de ação do usuariom etc)
 const EventEmitter = require('events')
 class MeuEmissor extends EventEmitter{
 
@@ -16,9 +18,17 @@ meuEmissor.emit(nomeEvento, 'na barra de rolagem')
 meuEmissor.emit(nomeEvento, 'no botão OK')
 
 
-//emissão de eventos em loop
-let count = 0
+// //emissão de eventos em loop
+// let count = 0
 
-setInterval(() => {
-    meuEmissor.emit(nomeEvento, 'no botão OK ' + (count++))
-}, 250);
+// setInterval(() => {
+//     meuEmissor.emit(nomeEvento, 'no botão OK ' + (count++))
+// }, 250);
+
+
+//LENDO O STDIN
+const stdin = process.openStdin()
+stdin.addListener('data', function(value){
+    console.log(`Você digitou: ${value.toString().trim()}` )
+})
+
